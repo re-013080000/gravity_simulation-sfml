69559b5cc90dff3cb3835a23f7d4184083804ad1
re.013080000 <104734867+re-013080000@users.noreply.github.com>
2022-06-07 00:44:02 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,7 @@
 
 
 
-			//caractéristiques du soleil
+			//characteristics of the sun
 //-----------------------------------------------------------------------------------------------
 class GravitySource {
 	sf::Vector2f position;
@@ -13,32 +13,36 @@ class GravitySource {
 	float strength;
 
 public:
-	GravitySource(float x_location, float y_location, float strength, float x_velocity, float y_velocity) { //position x,y ; force d'attraction ; vélocité x,y
+	GravitySource(float x_location, float y_location, float strength, float x_velocity, float y_velocity) { //position x,y ; force of attraction ; velocity x,y
 		position.x = x_location;
 		position.y = y_location;
 		velocity.x = x_velocity;
 		velocity.y = y_velocity;
 		this->strength = strength;
 
-		planet.setPosition(position); //place le soleil aux coordonnées prédéfinies
+		planet.setPosition(position); //places the sun at the predefined coordinates
 		planet.setFillColor(sf::Color::Yellow);
-		planet.setRadius(20); //Diamètre du soleil: 40
+		planet.setRadius(20); //diameter of the sun: 40
 	}
 
 	void render(sf::RenderWindow& window) {
-		window.draw(planet); //affiche le soleil à l'écran
+		window.draw(planet); //displays the sun on the screen
 	}
 
 	sf::Vector2f get_pos() {
 		planet.setPosition(position);
-		return position; //renvoie la position du soleil (x,y)
+		return position; //returns the position of the sun (x,y)
+	}
+
+	sf::FloatRect get_bd() {
+		return planet.getGlobalBounds(); //return current hitbox
 	}
 
 	float get_strength() {
-		return strength; //renvoie la force d'attractivité du soleil
+		return strength; //returns the attractiveness of the sun
 	}
 
-	void update_physics(GravitySource& planet) { //le soleil avance d'une vitesse constante
+	void update_physics(GravitySource& planet) { //the sun advances at a constant speed
 		position.x += velocity.x;
 		position.y += velocity.y;
 	}
@@ -48,7 +52,7 @@ public:
 
 
 
-			//caractéristiques des planètes
+			//characteristics of the planets
 //-----------------------------------------------------------------------------------------------
 class Particle {
 	sf::Vector2f position;
@@ -56,30 +60,38 @@ class Particle {
 	sf::CircleShape planet;
 
 public:
-	Particle(float x_location, float y_location, float velocity_x, float velocity_y) { //position x,y ; velocité x,y
+	Particle(float x_location, float y_location, float velocity_x, float velocity_y, int color) { //location x,y ; velocity x,y
 		position.x = x_location;
 		position.y = y_location;
 		velocity.x = velocity_x;
 		velocity.y = velocity_y;
 
-		planet.setPosition(position); //place la planète aux coordonnées prédéfinies
-		planet.setFillColor(sf::Color::Cyan);
-		planet.setRadius(4); //diamètre de chaque planète: 4
+		planet.setPosition(position); //places the planet at the predefined coordinates
+		planet.setFillColor(sf::Color(85, color*1.8, color));
+		planet.setRadius(4); //diameter of each planet: 4
 	}
 
-	void render(sf::RenderWindow& window) { //affiche la planète à l'écran
+	void render(sf::RenderWindow& window) { //dispaly the planet
 		planet.setPosition(position);
 		window.draw(planet);
 	}
 
-	void update_physics(GravitySource& planet) { //met à jour les nouvelles coordonnées de la planète
-		float distance_x = planet.get_pos().x - position.x; //distance x entre le soleil et la planète
-		float distance_y = planet.get_pos().y - position.y; //distance y entre le soleil et la planète
+	sf::FloatRect get_bd() {
+		return planet.getGlobalBounds(); //return current hitbox
+	}
 
-		float distance = sqrt(distance_x * distance_x + distance_y * distance_y); //théorème de Pythagore - distance la plus courte entre le soleil et la planète
+	void burnt() {
+		planet.setFillColor(sf::Color(0, 0, 0));
+	}
+
+	void update_physics(GravitySource& planet) { //updates the new coordinates of the planet
+		float distance_x = planet.get_pos().x - position.x; //distance x between the sun and the planet
+		float distance_y = planet.get_pos().y - position.y; //distance y between the sun and the planet
+
+		float distance = sqrt(distance_x * distance_x + distance_y * distance_y); //Pythagorean theorem - shortest distance between the sun and the planet
 
 
-					//inverse-square law  -  La force d'attraction gravitationnelle entre deux masses ponctuelles est directement proportionnelle au produit de leurs masses et inversement proportionnelle au carré de leur distance de séparation
+					//inverse-square law  -  The gravitational attraction force between two point masses is directly proportional to the product of their masses and inversely proportional to the square of their separation distance
 		//---------------------------------------------------
 		float inverse_distance = 1.f / distance;
 
@@ -93,10 +105,10 @@ public:
 		//---------------------------------------------------
 
 
-		velocity.x += acceleration_x; //vélocité x,y changeant en fonction de la distance entre le soleil et la planète
+		velocity.x += acceleration_x; //velocity x,y changing according to the distance between the sun and the planet
 		velocity.y += acceleration_y;
 
-		position.x += velocity.x; //nouvelle position x,y de la planète
+		position.x += velocity.x; //new location / mouvment
 		position.y += velocity.y;
 	}
 };
@@ -106,6 +118,7 @@ public:
 
 
 int main() {
+	srand(time(0));
 	std::cout << R"(
         *******************************************************
                       __  _ ____ __  ___  __   __   __   __  
@@ -119,10 +132,25 @@ int main() {
 	sf::RenderWindow window(sf::VideoMode(1600, 1200), "Gravity_Simulation");
 	window.setFramerateLimit(60);
 
-	GravitySource source(1300, 800, 5000, -0.5, -0.15);
-	Particle particle(1550, 700, -4, -2);
-	Particle particle2(1450, 600, -2, -5);
-	Particle particle3(1500, 550, 1, 3);
+	GravitySource source(1300, 800, 7000, -0.5, -0.15); //setting up the sun | position_x, position_y, strength, velocity_x, velocity_y
+
+
+	//create a certain amount of planets
+	Particle* temp;
+	std::vector<Particle*> Galaxy;
+
+	for (int i = 0;i < 10000; i++) {
+		int color = rand() % 255;
+		if (color * 1.2 > 255) { //random color for planets
+			color = color * 3 - 255;
+		}
+		int x_rand = rand() % 1600; //random position x,y for planets
+		int y_rand = rand() % 1200;
+
+		temp = new Particle(x_rand, y_rand, 3, -0.5f, color); //var temp for brand new planets
+		Galaxy.push_back(temp); //planets stocked in the array/list/vector 'Galaxy'
+	}
+
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -133,15 +161,22 @@ int main() {
 
 		window.clear();
 
-		particle.update_physics(source);
-		particle2.update_physics(source);
-		particle3.update_physics(source);
+		sf::FloatRect sun = source.get_bd(); //get the sun's hitbox
+
 		source.update_physics(source);
 
+		//display the planets at screen
+		for (int i = 0; i < 10000; i++) {
+			sf::FloatRect planet = Galaxy[i]->get_bd(); //get each planet's hitbox
+			if (sun.intersects(planet)) { //delete the planet from the universe if that one touches the sun
+				std::vector<Particle*> erase(i); //remove from the list
+				Galaxy[i]->burnt(); //remove from the screen (paints it in black)
+			}
+			Galaxy[i]->update_physics(source);
+			Galaxy[i]->render(window);
+		}
+
 		source.render(window);
-		particle.render(window);
-		particle2.render(window);
-		particle3.render(window);
 
 		window.display();
 	}
